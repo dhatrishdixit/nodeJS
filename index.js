9854389b7190c8bb075e9e508ffbe4e1aa662d4e
dhatrishdixit <dhatrish29@gmail.com>
2024-12-15 23:28:00 +0530

    learning more and more

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const addFn = require("./modules/modules.js");
+// const addFn = require("./modules/modules.js");
+// const addFn2 = require("./modules/modules.js");
+
+// console.log(__filename);
+// console.log(addFn(1,2));
+// console.log("inside index.js"); 
 
 
-console.log(addFn(1,2));
-console.log("inside index.js"); 
+//this showcases caching in modulation 
+const superHero = require("./modules/superHeroClass");
+
+console.log(superHero.getName());
+console.log(superHero.setName("superman"));
+console.log(superHero.getName());
+
+// here module caching causes issue to us 
+// to get rid of it we should export class directly 
+const superHero2 = require("./modules/superHeroClass");
+
+console.log(superHero2.getName());
--- modules/iife.js
+++ modules/iife.js
@@ -1,4 +1,14 @@
-(function(){
-    console.log("this is inside iife");
+(function(intro){
+    const superHero = "superman";
+    console.log(`${intro} this is ${superHero}`);
+    console.log("this is inside iife 1");
     // this is the way modules scope work 
-})()
+})("hey");
+
+(function(intro){
+    const superHero = "batman";
+    console.log(`${intro} this is ${superHero}`)
+    console.log("this is inside iife 2");
+    // this is the way modules scope work 
+})("hello");
+
