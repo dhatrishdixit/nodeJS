2d1ed30073c2eace509f454048f915e77ff2b450
dhatrishdixit <dhatrish29@gmail.com>
2024-12-16 12:28:11 +0530

    esm modules

---
+++ modules/main.mjs
@@ -0,0 +1,17 @@
+//import math from "./math-esm.mjs";
+
+// const {add,subtract} = math;
+// this for default imports 
+
+//import * as math from "./math-esm.mjs"
+
+// const {add,subtract} = math;
+
+// and 
+
+import {add,subtract} from "./math-esm.mjs"
+// this for named imports
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+
---
+++ modules/math-esm.mjs
@@ -0,0 +1,7 @@
+export const add = (a,b) => a+b;
+
+export const subtract = (a,b) => Math.abs(a-b);
+
+// export default {
+//     add , subtract
+// }
