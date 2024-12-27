1e631a875bbce1de5bb740ebfb61a0b5fcbf8460
dhatrishdixit <dhatrish29@gmail.com>
2024-12-28 01:10:38 +0530

    learnt about characterSets

---
+++ characterSets.js
@@ -0,0 +1,12 @@
+// first convert a character to a number
+// character sets - predefined set of characters represented by numbers 
+const letter = "a";
+console.log(letter.charCodeAt());
+// popular character sets - ASCII and Unicode 
+// in nodeJS it is Unicode
+
+// character encoding - how to convert a number in a character set as binary data before it is stored in a computer 
+// it dictates how many bits are used to represent a number 
+// popular character encoding - UTF-8, UTF-16, UTF-32
+// in nodeJS it is UTF-8
+// UTF-8 is a variable length encoding scheme that uses 8-bit code units , i.e. 1 byte 
