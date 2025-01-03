786ea74b73baab4231277bcce474f244728bc4a9
dhatrishdixit <dhatrish29@gmail.com>
2025-01-04 02:11:51 +0530

    learnt more about libuv

--- builtIn-modules/libuv/exampleAsync.js
+++ builtIn-modules/libuv/exampleAsync.js
@@ -1,5 +1,8 @@
 const crypto = require("node:crypto");
+const os = require('node:os');
 
+console.log(os.cpus().length);
+process.env.UV_THREADPOOL_SIZE = 6;
 const start = Date.now();
 const totalHash = 6;
 
@@ -10,4 +13,10 @@ for(let i=1;i<=totalHash;i++){
 }
 
 // in this multiple threads are created to handle the async operations apart from main thread 
-// and you can see the limitation of this as my compute has only 2 cores and that is 4 threads so after that it will be queued and will be executed after the previous ones are done.
+// and you can see the limitation of this as my computer has only 2 cores and that is 4 threads at max so after that it will be queued and will be executed after the previous ones are done , when we increase max threads more than the number of cores performance will degrade 
+
+// at max a core can handle two threads 
+
+// libuv is by default set at 4 threads and we can increase it by setting the environment variable UV_THREADPOOL_SIZE to the number of threads we want to use
+
+// A few async methods like fs.readFile and crypto.pbkdf2 run on a separate thread in lubuv's thread pool. They do run synchronously in their own thread, but as far as the main thread is concerned they appear non blocking in nature .
