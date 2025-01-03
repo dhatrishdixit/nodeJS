d22f916659925fff5c424994f05c76323295332f
dhatrishdixit <dhatrish29@gmail.com>
2025-01-03 23:49:08 +0530

    learnt more about threads in my pc

--- builtIn-modules/libuv/exampleSync.js
+++ builtIn-modules/libuv/exampleSync.js
@@ -7,3 +7,5 @@ crypto.pbkdf2Sync("password","salt", 100000, 512, "sha512");
 console.log("hash 2:", Date.now() - start, "ms");
 crypto.pbkdf2Sync("password","salt", 100000, 512, "sha512");
 console.log("hash 3:", Date.now() - start, "ms");
+
+// sync operations are blocking the main thread and it is not recommended to use sync operations in node.js
